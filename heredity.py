049cf8c82dc91616e25dcf30f374da966d194a2c
czlight <alejandro.r1212@gmail.com>
2024-09-04 19:27:56 +0000

    Wed, Sep 4, 2024, 2:27 PM -05:00

--- heredity.py
+++ heredity.py
@@ -230,9 +230,11 @@ def joint_probability(people, one_gene, two_genes, have_trait):
         elif (person not in one_gene and person not in two_genes) and person not in have_trait:
             probabilities[person] *= PROBS["trait"][0][False]
 
-        
 
-        print(probabilities)
+
+        print("end of looP!",probabilities)
+
+    totalProbabilities = 
 
 
 
