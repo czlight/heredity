a4856f662d9ff6d71507cb24395a2d55e33fafe3
czlight <alejandro.r1212@gmail.com>
2024-09-05 19:51:52 +0000

    Thu, Sep 5, 2024, 2:51 PM -05:00

--- heredity.py
+++ heredity.py
@@ -235,7 +235,7 @@ def joint_probability(people, one_gene, two_genes, have_trait):
                 # inherit one gene
             else:
                 #inherit no genes
-                probabilities[person] =(.49 * .99) + (.99 * .01)
+                probabilities[person] =(.5 * .01 * .99) + (.99 * .01)
 
 
         # neither parent has gene (will this run for people without parents? if it does it will )
