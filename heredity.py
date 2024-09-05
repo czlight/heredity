252f50b4c69887339dafc6a46c2303e4956a1605
czlight <alejandro.r1212@gmail.com>
2024-09-05 22:16:55 +0000

    Thu, Sep 5, 2024, 5:16 PM -05:00

--- heredity.py
+++ heredity.py
@@ -226,11 +226,12 @@ def joint_probability(people, one_gene, two_genes, have_trait):
                 probabilities[person] = (.5 * (1 - PROBS["mutation"])) * (.5 * (1 - PROBS["mutation"])) # = .245025
 
             if person in one_gene:
-                probabilities[person] = (.5 * .99 ) + (.5 * .99) # = 
+                probabilities[person] = (.5 * .99) * (.5 * .99) # =.245025
                 # inherit one gene
             else:
                 #inherit no genes; each parent has one gene out of two, so the probability of not inheriting the gene from each parent is 1/2: 1/2 *1/2 = 1/4
                 probabilities[person] = (.5 * (1 - PROBS["mutation"])) * (.5 * (1 - PROBS["mutation"]))
+                # 
 
         # one parent has one gene, the other parent has none
         elif (people[person]["father"] in one_gene and people[person]["mother"] not in two_genes and people[person]["mother"] not in one_gene) or \
